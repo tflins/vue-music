c83b2c3c767cd8948b2abae0717de98af9714c4b
tanfenglin <tanfl@uxsino.com>
2018-12-25 14:16:45 +0800

    完成首页m-header

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <div id="app">
-    hello
+    <m-header></m-header>
   </div>
 </template>
 
 <script>
+import MHeader from '@/components/MHeader/MHeader'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    MHeader
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "@/common/scss/const.scss";
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: color-text;
   margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import FastClick from 'fastclick'
 // import router from './router'
 
 import '@/common/scss/index.scss'
+
+// 解决移动端3秒延迟
+FastClick.attach(document.body)
 
 Vue.config.productionTip = false
 
