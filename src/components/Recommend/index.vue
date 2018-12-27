4a37ce6ef8782ccc36de3689406ac1d8ea736f88
tflin <tflins@163.com>
2018-12-28 00:04:44 +0800

    推荐组件数据获取

--- src/components/Recommend/index.vue
+++ src/components/Recommend/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="recommend">
-    <h1>推荐</h1>
+    <div class="recommend-content">
+      <div class="slider-wrapper"></div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul></ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,9 +24,6 @@ export default {
       getRecommend().then(res => {
         if (res.code === ERR_OK) {
           console.log(res)
-        } else {
-          console.log('出错率');
-          
         }
       })
     }
@@ -29,12 +32,62 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "@/common/scss/mixin.scss";
 @import "@/common/scss/const.scss";
 
 .recommend {
-  h1 {
-    color: $color-text;
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+  .recommend-content {
+    height: 100%;
+    overflow: hidden;
+    .slider-wrapper {
+      position: relative;
+      overflow: hidden;
+    }
+    .recommend-list {
+      .list-title {
+        height: 65px;
+        line-height: 65px;
+        text-align: center;
+        font-size: $font-size-medium;
+        color: $color-theme;
+      }
+      .item {
+        display: flex;
+        box-sizing: border-box;
+        align-items: center;
+        padding: 0 20px 20px 20px;
+        .icon {
+          flex: 0 0 60px;
+          width: 60px;
+          padding-right: 20px;
+        }
+        .text {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          flex: 1;
+          line-height: 20px;
+          overflow: hidden;
+          font-size: $font-size-medium;
+          .name {
+            margin-bottom: 10px;
+            color: $color-text;
+          }
+          .desc {
+            color: $color-text-d;
+          }
+        }
+      }
+      .loading-content {
+        position: absolute;
+        width: 100%;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+    }
   }
 }
 </style>
