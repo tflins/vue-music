bcf8229d99855426d5eaa615f51da71ef73c9f87
tflin <tflins@163.com>
2018-12-28 00:07:57 +0800

    格式化tab组件代码

--- src/components/Tab/index.vue
+++ src/components/Tab/index.vue
@@ -1,31 +1,15 @@
 <template>
   <div class="tab">
-    <router-link
-      tag="div"
-      class="tab-item"
-      to="/recommend"
-    >
+    <router-link tag="div" class="tab-item" to="/recommend">
       <span class="tab-link">推荐</span>
     </router-link>
-    <router-link
-      tag="div"
-      class="tab-item"
-      to="/singer"
-    >
+    <router-link tag="div" class="tab-item" to="/singer">
       <span class="tab-link">歌手</span>
     </router-link>
-    <router-link
-      tag="div"
-      class="tab-item"
-      to="/rank"
-    >
+    <router-link tag="div" class="tab-item" to="/rank">
       <span class="tab-link">排行</span>
     </router-link>
-    <router-link
-      tag="div"
-      class="tab-item"
-      to="/search"
-    >
+    <router-link tag="div" class="tab-item" to="/search">
       <span class="tab-link">搜索</span>
     </router-link>
   </div>
