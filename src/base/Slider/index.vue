7863b0d36d98e4538cfb0a70b3bf0a9e122dd289
tflin <tflins@163.com>
2018-12-28 22:41:44 +0800

    完成轮播图组件

---
+++ src/base/Slider/index.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
+      <slot></slot>
+    </div>
+    <div class="dots"></div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+import {addClass} from '@/common/js/dom'
+
+export default {
+  props: {
+    loop: {
+      type: Boolean,
+      default: true
+    },
+    autoPlay: {
+      type: Boolean,
+      default: true
+    },
+    interval: {
+      type: Number,
+      default: 4000
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this._setSliderWidth()
+      this._initSlider()
+    }, 20)
+  },
+  methods: {
+    _setSliderWidth() {
+      this.children = this.$refs.sliderGroup.children
+
+      let width = 0
+      let sliderWidth = this.$refs.slider.clientWidth
+      console.log(this.children.length)
+      for (let i = 0, len = this.children.length; i < len; i++) {
+        let child = this.children[i]
+        addClass(child, 'slider-item')
+
+        child.style.width = sliderWidth + 'px'
+        console.log(child.style.width)
+
+        width += sliderWidth
+      }
+
+      if (this.loop) {
+        width += sliderWidth * 2
+      }
+      this.$refs.sliderGroup.style.width = width + 'px'
+    },
+    _initSlider() {
+      this.slider = new BScroll(this.$refs.slider, {
+        scrollX: true,
+        scrollY: false,
+        momentum: false,
+        snap: {
+          loop: this.loop
+        },
+        snapThreshold: 0.3,
+        snapSpeed: 400
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/common/scss/const.scss";
+
+.slider {
+  min-height: 1px;
+  .slider-group {
+    position: relative;
+    overflow: hidden;
+    white-space: nowrap;
+    .slider-item {
+      float: left;
+      box-sizing: border-box;
+      overflow: hidden;
+      text-align: center;
+      a {
+        display: block;
+        width: 100%;
+        overflow: hidden;
+        text-decoration: none;
+      }
+      img {
+        display: block;
+        width: 100%;
+      }
+    }
+  }
+  .dots {
+    position: absolute;
+    right: 0;
+    left: 0;
+    bottom: 12px;
+    text-align: center;
+    font-size: 0;
+    .dot {
+      display: inline-block;
+      margin: 0 4px;
+      width: 8px;
+      height: 8px;
+      border-radius: 50%;
+      background-color: $color-text-l;
+      &.active {
+        width: 20%;
+        border-radius: 5px;
+        background-color: $color-text-ll;
+      }
+    }
+  }
+}
+</style>
--- src/components/Recommend/index.vue
+++ src/components/Recommend/index.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="recommend">
     <div class="recommend-content">
-      <div class="slider-wrapper"></div>
+      <div v-if="recommends.length" class="slider-wrapper">
+        <slider>
+          <div v-for="item in recommends" :key="item.id">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl">
+            </a>
+          </div>
+        </slider>
+      </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
         <ul></ul>
@@ -12,6 +20,7 @@
 
 <script>
 import {getRecommend} from '@/api/recommend'
+import Slider from '@/base/Slider'
 import {ERR_OK} from '@/api/config'
 
 export default {
@@ -19,14 +28,22 @@ export default {
   created() {
     this._getRecommend()
   },
+  data() {
+    return {
+      recommends: []
+    }
+  },
   methods: {
     _getRecommend() {
       getRecommend().then(res => {
         if (res.code === ERR_OK) {
-          console.log(res)
+          this.recommends = res.data.slider
         }
       })
     }
+  },
+  components: {
+    Slider
   }
 }
 </script>
