5cb0238d190adef622bc54c7b7d052eebcbab99d
tflin <tflins@163.com>
2019-03-16 12:53:13 +0800

    播放器vuex数据设计

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,19 @@
+import { playMode } from '@/common/js/config'
+
 const state = {
-  singer: {}
+  singer: {},
+  // 正在播放中
+  playing: false,
+  // 播放器展开或收起
+  fullScreen: false,
+  // 播放列表
+  playList: [],
+  // 歌曲顺序列表
+  sequenceList: [],
+  // 播放模式
+  mode: playMode,
+  // 当前播放索引
+  currentIndex: -1
 }
 
 export default state
